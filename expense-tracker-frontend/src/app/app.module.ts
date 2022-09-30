cead3bb984a16a5b27767a16ad7d6080bdd60264
Martin Mbithi <martdevelopers254@gmail.com>
2022-09-30 16:55:31 +0300

    Get Response From Expense Services

--- expense-tracker-frontend/src/app/app.module.ts
+++ expense-tracker-frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ListExpenseComponent } from './components/list-expense/list-expense.component';
 
@@ -10,7 +10,8 @@ import { ListExpenseComponent } from './components/list-expense/list-expense.com
     ListExpenseComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- expense-tracker-frontend/src/app/services/expense.service.ts
+++ expense-tracker-frontend/src/app/services/expense.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Expense } from '../models/expense';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExpenseService {
 
-  constructor() { }
+  //Register Your Url Here
+  private getUrl: string = "http://127.0.0.1:8080/api/v1/expenses"
+  constructor(private _httpClient: HttpClient) {   }
+
+  getExpenses(): Observable<Expense[]> {
+    return this._httpClient.get<Expense[]>(this.getUrl).pipe(
+      map(response => response )
+    )
+    
+  }
 }

3b970c5c6d6adbf9c6227937ac8338508b680b1d
Martin Mbithi <martdevelopers254@gmail.com>
2022-09-30 17:11:54 +0300

    Allow Cross Origin

--- src/main/java/com/devlan/expense/controller/ExpenseController.java
+++ src/main/java/com/devlan/expense/controller/ExpenseController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.devlan.expense.model.Expense;
 import com.devlan.expense.service.ExpenseService;
  
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1")
 public class ExpenseController {

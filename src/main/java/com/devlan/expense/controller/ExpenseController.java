e5080c0a5e2a91600c764697faabba9d0275b6c4
Martin Mbithi <martdevelopers254@gmail.com>
2022-09-30 14:53:16 +0300

    Create Response Controller And Expose REST Endpoint Returning List Of Expenses

---
+++ src/main/java/com/devlan/expense/controller/ExpenseController.java
@@ -0,0 +1,27 @@
+package com.devlan.expense.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devlan.expense.model.Expense;
+import com.devlan.expense.service.ExpenseService;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ExpenseController {
+    @Autowired
+    ExpenseService expenseService;
+    
+    @GetMapping("/expenses")
+    public ResponseEntity<List<Expense>> get(){
+        List<Expense> expenses = expenseService.findAll();   
+        return new ResponseEntity<List<Expense>>(expenses, HttpStatus.OK);
+        
+    }
+}

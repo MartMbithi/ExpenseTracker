77b4197e02019f6d09d7f0b20e4c23d1df2aeb37
Martin Mbithi <martdevelopers254@gmail.com>
2022-09-30 16:36:37 +0300

    Register List Expense Componet In App Component File

--- expense-tracker-frontend/src/app/app.module.ts
+++ expense-tracker-frontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListExpenseComponent } from './components/list-expense/list-expense.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListExpenseComponent
   ],
   imports: [
     BrowserModule
---
+++ expense-tracker-frontend/src/app/components/list-expense/list-expense.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-expense',
+  templateUrl: './list-expense.component.html',
+  styleUrls: ['./list-expense.component.css']
+})
+export class ListExpenseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

1ed6cb0ec86009b5b4562261cc825bc53f070077
Martin Mbithi <martdevelopers254@gmail.com>
2022-10-03 18:03:08 +0300

    Implement Add Expense Component With Save Expense Endpoint

--- expense-tracker-frontend/src/app/components/add-expense/add-expense.component.ts
+++ expense-tracker-frontend/src/app/components/add-expense/add-expense.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Expense } from 'src/app/models/expense';
+import { ExpenseService } from 'src/app/services/expense.service';
 
 @Component({
   selector: 'app-add-expense',
   templateUrl: './add-expense.component.html',
-  styleUrls: ['./add-expense.component.css']
+  styleUrls: ['./add-expense.component.css'],
 })
 export class AddExpenseComponent implements OnInit {
+  expense: Expense = new Expense();
 
-  constructor() { }
+  constructor(
+    private _expenseService: ExpenseService,
+    private _router: Router
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  //Save Expense
+  saveExpense() {
+    this._expenseService.saveExpense(this.expense).subscribe((data) => {
+      console.log('response', data);
+      this._router.navigateByUrl('/expenses');
+    });
   }
-
 }

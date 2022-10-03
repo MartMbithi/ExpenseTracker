3b2f2d473aa57576da1e837dc823f692c4057c9a
Martin Mbithi <martdevelopers254@gmail.com>
2022-10-03 14:27:14 +0300

    Regiseter Router Module

--- expense-tracker-frontend/src/app/app.module.ts
+++ expense-tracker-frontend/src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
+import {RouterModule, Routes} from "@angular/router"
 import { AppComponent } from './app.component';
 import { ListExpenseComponent } from './components/list-expense/list-expense.component';
 import { AddExpenseComponent } from './components/add-expense/add-expense.component';
+
+const routers: Routes  = [
+  {path: 'expenses', component: ListExpenseComponent}
+];
 
 @NgModule({
   declarations: [

7db4e35b1f488c1ccd6b0b9a3b18e8f7e2f292f3
Martin Mbithi <martdevelopers254@gmail.com>
2022-10-03 14:24:25 +0300

    Generate Add Expense Components

--- expense-tracker-frontend/src/app/app.module.ts
+++ expense-tracker-frontend/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ListExpenseComponent } from './components/list-expense/list-expense.component';
+import { AddExpenseComponent } from './components/add-expense/add-expense.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListExpenseComponent
+    ListExpenseComponent,
+    AddExpenseComponent
   ],
   imports: [
     BrowserModule,

7c298ac267b4af9f2b4fe502b0bb86987806ee86
Martin Mbithi <martdevelopers254@gmail.com>
2022-10-04 12:10:39 +0300

    Register Delete Mapping

--- src/main/java/com/devlan/expense/controller/ExpenseController.java
+++ src/main/java/com/devlan/expense/controller/ExpenseController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,6 +44,14 @@ public class ExpenseController {
         return new ResponseEntity<Expense>(expense, HttpStatus.OK);
         
     }
+    
+    @DeleteMapping("/expenses/{id}")
+    public ResponseEntity<String> delete(@PathVariable("id") Long id){
+        expenseService.delete(id);   
+        return new ResponseEntity<String>("Expense is deleted successfully", HttpStatus.OK);
+        
+    }   
+    
     
     
 }
